e98904dde9857531077f5a8b92792e370164c123
Luis Enrique Vega Martinez <luis.vega@sourcemeridian.com>
2025-10-20 12:23:29 -0500

    add text message components and integrate them into the orthography page

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,15 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent } from '@components';
-
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TextMessageBoxEvent, TextMessageEvent, TypingLoaderComponent } from '@components';
+import { Message } from '@interfaces';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-orthography-page',
   imports: [
     ChatMessageComponent,
     MyMessageComponent,
-    TypingLoaderComponent
+    TypingLoaderComponent,
+    TextMessageBoxComponent
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent { }
+export default class OrthographyPageComponent {
+
+  public messages = signal<Message[]>([{ text: 'Hola Mundo', isGpt: false }]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+
+
+  handleMessage(prompt: string) {
+
+    console.log({ prompt });
+
+  }
+
+  handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+
+    console.log({ prompt, file });
+
+  }
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    console.log(event);
+  }
+}
